96c1fa688fa21dad7cac553c619487c244b03bda
Eric Garcia <78274657+Egr711@users.noreply.github.com>
2021-02-14 15:36:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Signup from "./Containers/Signup"
 class App extends Component{
     render(){
         return(
-            <Router>
+            <Router>        //router is used to link pages together,ex:if u want a button to link to a certain page you have to add the path 
                 <Switch>
-                    <Route exact path="/" component={Login}></Route>
+                    <Route exact path="/" component={Login}></Route>    //main page (login page)
                     <Route exact path="/Signup" component={Signup}></Route>
                 </Switch>
             </Router>
