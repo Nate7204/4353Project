548162a680cea16ccf2f18afa8c101ea2d291a47
Eric Garcia <78274657+Egr711@users.noreply.github.com>
2021-02-26 21:10:40 -0600

    Added fuel route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, {Component} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Login from './Containers/Login'
 import Signup from "./Containers/Signup"
+import History from './Containers/History'
+import WelcomePage from './Containers/WelcomePage'
+import ProfileCompletion from './Containers/ProfileCompletion'
+import Fuel from './Containers/Fuel'
 
 class App extends Component{
     render(){
         return(
-            <Router>        //router is used to link pages together,ex:if u want a button to link to a certain page you have to add the path 
+            <Router>
                 <Switch>
-                    <Route exact path="/" component={Login}></Route>    //main page (login page)
+                    <Route exact path="/" component={Login}></Route>
                     <Route exact path="/Signup" component={Signup}></Route>
+                    <Route exact path="/History" component={History}></Route>
+                    <Route exact path="/WelcomePage" component={WelcomePage}></Route>
+                    <Route exact path="/ProfileCompletion" component={ProfileCompletion}></Route>
+                    <Route exact path="/Fuel" component={Fuel}></Route>
                 </Switch>
             </Router>
         )
