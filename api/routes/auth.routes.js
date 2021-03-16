6bef904e557aa8efb4437388a76844e9319142a8
Eric Garcia <78274657+Egr711@users.noreply.github.com>
2021-03-16 16:54:10 -0500

    Cleaning up code

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -11,22 +11,3 @@ router.post("/api/auth/signup", controller.signup)
 
 
 module.exports = router;
-/*
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-
-  app.post(
-    "/api/auth/signup",
-    [
-      verifySignUp.checkDuplicateUsernameOrEmail,
-    ],
-    controller.signup
-  );
-};
-*/
