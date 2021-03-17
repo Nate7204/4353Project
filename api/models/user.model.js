2241af19be0cd38d0667aef4a364679502f24e95
Eric Garcia <78274657+Egr711@users.noreply.github.com>
2021-03-16 19:20:18 -0500

    moved temporary variables from auth.controller.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,8 +1,9 @@
 //this is where ur sopposed to call the database 
-module.exports = () => {
-    var Users = [{
-        username: "theusername",
-        password: "password1"
-    }]
-    return Users
-}
+
+var user = [{
+    username: "theusername",
+    password: "password1",
+    newUser: false
+}]
+
+module.exports = user
