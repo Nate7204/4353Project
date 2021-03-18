64bbd04bb7afd090c11f91502e1ba76afe7531ed
EAJ2 <78275071+EAJ2@users.noreply.github.com>
2021-03-18 16:40:14 -0500

    Update auth.routes.js

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -5,9 +5,9 @@ const router = require("express").Router()
 router.post("/api/auth/signin", controller.signin)
 
 router.post("/api/auth/signup", controller.signup)
+router.post("/api/auth/RegisterForm", controller.profileform)
 //router.post("/api/auth/signup", [verifySignUp.checkDuplicateUsername], controller.signup)
 //checkDuplicateUsername currently does not work since usernames/passwords are saved locally
 //on auth.controller.js and verifyRegister.js cannot accesss those variable so it cant check if theyre duplicate
-
 
 module.exports = router;
