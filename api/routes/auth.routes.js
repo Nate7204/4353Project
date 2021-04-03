38b08458c440ffcb4fdc939bec9d59825498f940
Eric Garcia <78274657+Egr711@users.noreply.github.com>
2021-04-02 23:25:01 -0500

    removed getAddress

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -7,7 +7,6 @@ router.post("/api/auth/signup", controller.signup)
 router.post("/api/auth/profileform", controller.profileform)
 router.post("/api/auth/fuelInfo", controller.fuelInfo)
 router.post("/api/auth/fuelQuote", controller.fuelquote)
-router.post("/api/auth/getAddress", controller.getAddress)
 router.post("/api/auth/getHistory", controller.getHistory)
 
 module.exports = router;
